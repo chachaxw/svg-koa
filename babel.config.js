869de2c30bf1643fc95be59fbaa5b28c7a52ca51
chacha <867571123@qq.com>
2019-04-27 15:44:21 +0800

    Fix babel config error

--- babel.config.js
+++ babel.config.js
@@ -1,22 +1,8 @@
 module.exports = {
-  presets: [
-    [
-      "@babel/env",
-      {
-        "modules": false,
-        "targets": {
-          "node": true,
-          "browsers": ["> 1%", "last 2 versions", "not ie <= 8"]
-        },
-        "useBuiltIns": "usage"
-      }
-    ],
-    "@vue/app"
-  ],
+  presets: ["@babel/env"],
   plugins: [
     "@babel/plugin-transform-runtime",
     "@babel/plugin-proposal-object-rest-spread",
-    "@babel/plugin-transform-async-to-generator",
     [
       "component",
       {
