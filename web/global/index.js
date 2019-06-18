07415b1c502e58b8ecae0ddc6cb432d8c2217b40
chacha <867571123@qq.com>
2019-06-18 13:40:11 +0800

    Remove useless file

--- web/global/index.js
+++ web/global/index.js
@@ -1,3 +1,2 @@
 import './request';
-import './url-parse';
 import './fabric';
--- web/pages/app/index.js
+++ web/pages/app/index.js
@@ -1,7 +1,9 @@
 import homeApp from './home.vue';
 
 new Vue({
-    el: '#home-app',
-    template: '<homeApp/>',
-    components: {homeApp}
+  el: '#home-app',
+  template: '<homeApp/>',
+  components: {
+    homeApp
+  }
 });
