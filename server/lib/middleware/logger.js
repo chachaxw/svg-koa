ebbed7d8641afd0a29cd461df63a0662b9f3a010
chacha <867571123@qq.com>
2019-06-24 10:21:48 +0800

    Update proxy middleware

--- server/lib/middleware/logger.js
+++ server/lib/middleware/logger.js
@@ -3,8 +3,8 @@ const logger = require('koa-logger');
  * 日志
  */
 module.exports.default = module.exports = async (app) => {
-    app.use(logger((str, args) => {
-        console.log(str)
-    }));
-    app.context.logger.info('logger initialized');
+  app.use(logger((str, args) => {
+    console.log(str)
+  }));
+  app.context.logger.info('logger initialized');
 };
